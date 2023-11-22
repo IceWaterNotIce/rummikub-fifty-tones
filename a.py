bd30046b39f9343cca7a3607b1919301aae4fdba
IceWaterNotIce <icewaternotice@gmail.com>
2023-11-23 01:47:06 +0800

    Refactor tile handling logic and update file paths

--- a.py
+++ a.py
@@ -1,42 +1,10 @@
-vowel = ["a", "i" , "u", "e", "o"]
-consonant = ["","k", "s", "t", "n", "h", "m", "y", "r", "w"]
-fiftysounds = []
-a = []
+with open('tiles.txt', 'r') as f:
+    string_list = f.readlines()
 
-for i in range(5):
-    a = []
-    for j in range(10):
-        a.append(consonant[j] + vowel[i])
-    fiftysounds.append(a)
+# Remove newline characters from each line
+tiles = [line.strip() for line in string_list]
 
-print(fiftysounds)
-fiftysounds = [[sound for sound in row if sound not in ["yi", "ye","wi", "wu", "we"]] for row in fiftysounds]
-
-fiftysounds.append(["nn"])
-
-for i in fiftysounds:
-    for j in i:
-        print(j, end=" ")
-    print()
-
-print(sum(len(x) for x in fiftysounds)*4 +20)
-
-tiles = []
-for i in fiftysounds:
-    for j in i:
-        for k in range(4):
-            tiles.append(j) 
-for k in range(4):
-    tiles.append("°") 
-for k in range(4):
-    tiles.append("joker")
-for m in range(12):
-    tiles.append('"') 
-
-
-
-for i in tiles:
-    for j in i:
-        print(j, end=" ")
-    print()
-
+with open('tilesIndex.txt', 'w') as f:
+    for i in range(len(tiles)):
+        if i % 4 == 0:
+            f.write(tiles[i] + '\n')
